93d888a76a171db60027daba49bdfb2c2ae1c084
Spot204 <110775732+Spot204@users.noreply.github.com>
2024-12-20 23:47:48 +0700

    Delete python directory

--- python/job/bangcuuchuong.py
+++
@@ -1,7 +0,0 @@
-for i in range(1,10):
-    for j in range(1,11):
-        num = i*j
-        if j != 10:
-            print(i,'x',j,'=',num,end=",")
-        else :
-            print(i,'x',j,'=',num,".")
--- python/tuhoc/linearsearch.py
+++
@@ -1,7 +0,0 @@
-def linearsearch(list, terrget):
-    for i in range(0, len(list)):
-        if list[i] == terrget:
-            return i +1
-    return -1
-
-print(linearsearch([1,2,3,6], 6))
